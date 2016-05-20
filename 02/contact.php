6506c5bc90560203e50e5d50a92fe50ee308d6fa
honda <tuyo38383@yahoo.co.jp>
2016-05-20 18:11:16 +0900

    アラートを表示させたかったができなかったため、背景色などを決めて見やすくしました。

--- 02/contact.php
+++ 02/contact.php
@@ -1,29 +1,42 @@
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <link href="contact.css" rel="stylesheet" type="text/css">
+<html>
+<head>
+</head>
 
+<body bgcolor="#ffffe0">
+<?php
+    function check() {
+            echo $_POST['den'];
+            return false;
+    }
+?>
 
 <div id ="formWrap">
-<form action="result.php" method="post">
-<table class ="formTable">
+    <p align="center">
+<FONT size="7">お問い合わせ画面</FONT>
+    </p>
+<form action="result.php" method="post" onsubmit="return check()">
+<table class ="formTable" bgcolor="#F0FFF0">
     <tr>
 <label name="0" value="姓"><th>姓</th></label>
-<td><input type="txtbox" name="name1" value=""><br/><br/>
+<td><input type="txtbox" name="name1" value="" required><br/><br/>
 </td>
 </tr>
 
 <tr>
 <label name="1" value="名"><th>名</th></label>
 <td>
-<input type="txtbox" name="name2" value=""><br/><br/>
+<input type="txtbox" name="name2" value="" required><br/><br/>
 </td>
 </tr>
 
 <tr>
 <label name="2" value="性別"><th>性別</th></label>
 <td>
-<input type="radio" name="sei" value="男">男
-<input type="radio" name="sei" value="女"> 女
-<input type="radio" name="sei" value="不明"> 不明
+<input type="radio" name="sei" value="0">男
+<input type="radio" name="sei" value="1"> 女
+<input type="radio" name="sei" value="2"> 不明
 <br/><br/>
 </td>
 </tr>
@@ -52,9 +65,9 @@
 <tr>
 <label name="6" value="どこで知ったか"><th>どこで知ったか</th></label>
 <td>
-<input type="checkbox" name="doko[]" value="web">web
-<input type="checkbox" name="doko[]" value="チラシ">チラシ
-<input type="checkbox" name="doko[]" value="知り合い">知り合い
+<input type="checkbox" name="doko[]" value="0">web
+<input type="checkbox" name="doko[]" value="1">チラシ
+<input type="checkbox" name="doko[]" value="2">知り合い
 <br/><br/>
 </td>
 </tr>
@@ -63,9 +76,9 @@
 <label name="7" value="質問カテゴリ"><th>質問カテゴリ</th></label>
 <td>
 <select name="cate">
-<option value="研修について">研修について</option>
-<option value="配偶について">配偶について</option>
-<option value="福利厚生について">福利厚生について</option>
+<option value="0">研修について</option>
+<option value="1">配偶について</option>
+<option value="2">福利厚生について</option>
 </select><br/><br/>
 </td>
 </tr>
@@ -73,13 +86,16 @@
 <tr>
 <label name="8" value="質問内容"><th>質問内容</th></label>
 <td>
-<textarea type="txtbox" name="naiyo" value="" cols="30" rows="5"></textarea><br/><br/>
+<textarea type="txtbox" name="naiyo" value="" cols="50" rows="5"></textarea><br/><br/>
 </td>
 </tr>
 
 </table>
 <p align="center">
-    <input type="submit">
+    <input type="submit" name="sousin" onclick="<?php check() ?>">
+    <input type="reset" name="reset">
 </p>
 </form>
 </div>
+</body>
+</html>
