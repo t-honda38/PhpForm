d4f5d2a9e98e2459cdf1d7357ebee5406f075e09
honda <tuyo38383@yahoo.co.jp>
2016-05-25 17:51:27 +0900

    ボタンの大きさを変えて、背景の画像を表示するようにしました。今はデザインをどのようにするか考えている。

--- 02/contact2.php
+++ 02/contact2.php
@@ -36,11 +36,11 @@ header("Location: result.php");
 }
 ?>
 
-<body bgcolor="#ffffe0">
+<body background="P1030280-Edit.jpg">
 
 <div id ="formWrap">
     <p align="center">
-<FONT size="7">お問い合わせ画面</FONT>
+<FONT size="7" color="#F0FFF0">お問い合わせ画面</FONT>
     </p>
 <form action="contact2.php" method="post" name="form1">
 <table class ="formTable" bgcolor="#F0FFF0">
@@ -189,7 +189,7 @@ if (!empty($_POST["doko"])) {
 
 
 <p align="center">
-    <button type="submit" name="sousin">送信</button>
+    <button class="button1" type="submit" name="sousin">送信</button>
 </p>
 </form>
 </div>
